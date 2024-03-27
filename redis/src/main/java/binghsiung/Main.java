e99a386362414f476f9c569bc5e56b6ca04af361
xiong-bing <xiong_bing@outlook.com>
2024-03-28 00:29:58 +0800

    init:更新项目结构

---
+++ redis/src/main/java/binghsiung/Main.java
@@ -0,0 +1,14 @@
+package binghsiung;
+
+/**
+ * Created by xiongbing on 2024/3/27.
+ */
+public class Main {
+
+    private String a;
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
