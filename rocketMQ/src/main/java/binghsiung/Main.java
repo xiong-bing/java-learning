d4f43429b9da7a004ba970a662628af4d41bc20c
xiong-bing <xiong_bing@outlook.com>
2024-03-28 23:36:18 +0800

    init:更新项目结构

---
+++ rocketMQ/src/main/java/binghsiung/Main.java
@@ -0,0 +1,10 @@
+package binghsiung;
+
+/**
+ * Created by xiongbing on 2024/3/28.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
